55927a268a1c5ca9ccde80ff5f38be52f5555b06
Algovoid <87210332+algovoid@users.noreply.github.com>
2025-10-22 18:57:05 +0100

    Add LoggingService for application logging
    Introduces a static LoggingService class that initializes Serilog for console and file logging with daily rolling logs. Also provides a Shutdown method to flush and close the logger.

---
+++ Services/LoggingService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+using System.IO;
+
+namespace OfficeToPdf.Services
+{
+	public static class LoggingService
+	{
+		public static void Initialize()
+		{
+			Directory.CreateDirectory("logs");
+			Log.Logger = new LoggerConfiguration()
+				.WriteTo.Console()
+				.WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
+				.MinimumLevel.Debug()
+				.CreateLogger();
+		}
+
+		public static void Shutdown()
+		{
+			Log.CloseAndFlush();
+		}
+	}
+}
+
