2d512ad20a118b648b7b22be62849091df7e890b
Algovoid <87210332+algovoid@users.noreply.github.com>
2025-10-22 18:59:34 +0100

    Add ThreadFactory for creating STA threads
    Introduces a static ThreadFactory class with a method to create background threads in STA apartment state, simplifying thread creation for STA-dependent operations.

---
+++ Services/ThreadFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace OfficeToPdf.Services
+{
+	public static class ThreadFactory
+	{
+		public static Thread CreateSTAThread(ThreadStart action, string name)
+		{
+			var thread = new Thread(action)
+			{
+				IsBackground = true,
+				Name = name
+			};
+			thread.SetApartmentState(ApartmentState.STA);
+			return thread;
+		}
+	}
+}
+
