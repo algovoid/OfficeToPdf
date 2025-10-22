34beaffb3e68988c653b77faa3b439c735c94dcd
Algovoid <87210332+algovoid@users.noreply.github.com>
2025-10-22 18:59:21 +0100

    Remove unused using directives in WordConverter.cs
    Cleaned up the file by removing unnecessary using statements, leaving only the required namespaces for Word interop and project-specific models and services.

--- Converters/WordConverter.cs
+++ Converters/WordConverter.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using System;
-using Microsoft.Office.Interop.Word;
+﻿using Microsoft.Office.Interop.Word;
 using OfficeToPdf.Models;
 using OfficeToPdf.Services;
 
