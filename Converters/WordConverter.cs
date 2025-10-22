186d2d64a7eef9e1533edbf0f62b550f9fd883cb
Algovoid <87210332+algovoid@users.noreply.github.com>
2025-10-22 18:57:40 +0100

    Add WordConverter for Word to PDF conversion
    Introduces the WordConverter class implementing IOfficeConverter to convert Word documents to PDF using Microsoft.Office.Interop.Word. Includes resource management and application lifecycle handling.

---
+++ Converters/WordConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using Microsoft.Office.Interop.Word;
+using OfficeToPdf.Models;
+using OfficeToPdf.Services;
+
+namespace OfficeToPdf.Converters
+{
+	public class WordConverter : IOfficeConverter
+	{
+		private Application? _wordApp;
+
+		public OfficeDocumentType Type => OfficeDocumentType.Word;
+
+		public void Convert(string inputPath, string outputPath)
+		{
+			_wordApp ??= ApplicationSpawner.SpawnWord();
+			object inputFile = inputPath;
+			object outputFile = outputPath;
+			object missing = System.Type.Missing;
+
+			Document? doc = null;
+			try
+			{
+				doc = _wordApp.Documents.Open(ref inputFile, ref missing, ReadOnly: true);
+				doc.SaveAs2(outputFile, WdSaveFormat.wdFormatPDF);
+			}
+			finally
+			{
+				doc?.Close(false);
+				ApplicationSpawner.ReleaseComObject(doc);
+			}
+		}
+
+		public void Quit()
+		{
+			if (_wordApp != null)
+			{
+				ApplicationSpawner.QuitAndRelease(_wordApp, () => _wordApp.Quit(false));
+				_wordApp = null;
+			}
+		}
+	}
+}
+
