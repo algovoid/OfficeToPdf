3c3c7d413dbd82a93a6eba91160e691e1e26d791
Algovoid <87210332+algovoid@users.noreply.github.com>
2025-10-22 18:59:30 +0100

    Add JobQueue class for managing FileJob tasks
    Introduces the JobQueue class using BlockingCollection to enqueue and dequeue FileJob instances. This provides a thread-safe queue for job processing in the OfficeToPdf.Workers namespace.

---
+++ Workers/JobQueue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections.Concurrent;
+using OfficeToPdf.Models;
+
+namespace OfficeToPdf.Workers
+{
+	public class JobQueue
+	{
+		private readonly BlockingCollection<FileJob> _queue = new();
+
+		public void Enqueue(FileJob job) => _queue.Add(job);
+		public bool TryDequeue(out FileJob job) => _queue.TryTake(out job, 500);
+		public bool IsEmpty => _queue.Count == 0;
+	}
+}
