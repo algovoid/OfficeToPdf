e4ae907890cb207bc2e9a26929fc25b049d883a8
Algovoid <87210332+algovoid@users.noreply.github.com>
2025-10-22 18:57:31 +0100

    Add IOfficeConverter interface
    Introduces the IOfficeConverter interface in the Converters namespace, defining a contract for office document conversion with a Type property and a Convert method.

---
+++ Converters/IOfficeConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OfficeToPdf.Models;
+
+namespace OfficeToPdf.Converters
+{
+	public interface IOfficeConverter
+	{
+		OfficeDocumentType Type { get; }
+		void Convert(string inputPath, string outputPath);
+	}
+}
